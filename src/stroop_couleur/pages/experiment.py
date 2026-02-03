461768044915501fffe5719947be3b17d2b6cb86
guywiz <guywiz@gmail.com>
2026-02-03 11:03:00 +0100

    Update experiment.py
    reformatted using black

--- src/stroop_couleur/pages/experiment.py
+++ src/stroop_couleur/pages/experiment.py
@@ -5,11 +5,7 @@ import dash_bootstrap_components as dbc
 from dash import html, dcc, Input, Output, State, no_update, callback_context
 from dash.exceptions import PreventUpdate
 
-dash.register_page(
-    __name__,
-    path="/experiment",
-    title="Expérience – Stroop classique"
-)
+dash.register_page(__name__, path="/experiment", title="Expérience – Stroop classique")
 
 # Couleurs et mapping Dash (HTML)
 nb_trials = 10
@@ -18,7 +14,7 @@ COLORS = {
     "Rouge": "red",
     "Vert": "green",
     "Bleu": "blue",
-    "Jaune": "#ffda03" # jaune pas trop clair (parce que sur fond blanc)
+    "Jaune": "#ffda03",  # jaune pas trop clair (parce que sur fond blanc)
 }
 
 layout = html.Div(
@@ -26,23 +22,29 @@ layout = html.Div(
     children=[
         html.H2("Expérience Stroop"),
         html.P("Clique la couleur du texte, pas ce qui est écrit."),
-
         html.Div(id="stroop-word", style={"fontSize": "48px", "margin": "40px"}),
-
-        html.Div([
-            html.Button(c, id=f"btn-{c}", n_clicks=0,
-                        style={"fontSize": "24px", "margin": "10px"}) 
-            for c in COLORS.keys()
-        ]),
-
-        html.Button("Nouveau mot", id="new-word", 
-                    style={"marginTop": "40px", "fontSize": "20px"}),
-
+        html.Div(
+            [
+                html.Button(
+                    c,
+                    id=f"btn-{c}",
+                    n_clicks=0,
+                    style={"fontSize": "24px", "margin": "10px"},
+                )
+                for c in COLORS.keys()
+            ]
+        ),
+        html.Button(
+            "Nouveau mot",
+            id="new-word",
+            style={"marginTop": "40px", "fontSize": "20px"},
+        ),
         dcc.Store(id="trial-data"),
         dcc.Store(id="start-time"),
         dcc.Location(id="redirect_goodbye"),  # Pour la redirection à la fin
-    ]
+    ],
 )
+
 
 # Réception d’un clic d’un bouton couleur ou "Nouveau mot"
 @dash.callback(
@@ -50,11 +52,11 @@ layout = html.Div(
     Output("stroop-word", "style"),
     Output("trial-data", "data"),
     Output("start-time", "data"),
-	[Input(f"btn-{color}", "n_clicks") for color in COLORS.keys()],
+    [Input(f"btn-{color}", "n_clicks") for color in COLORS.keys()],
     Input("new-word", "n_clicks"),
     State("trial-data", "data"),
     State("start-time", "data"),
-	prevent_initial_call=True
+    prevent_initial_call=True,
 )
 def handle_event(color1, color2, color3, color4, current_res, trial, start_time):
     ctx = callback_context
@@ -69,10 +71,22 @@ def handle_event(color1, color2, color3, color4, current_res, trial, start_time)
         print(f"Received trial: {trial}\n")
         displayed_word = random.choice(list(COLORS.keys()))
         displayed_color = random.choice(list(COLORS.values()))
-        trial_data = {"displayed_word": displayed_word, "color": displayed_color, "congruent": displayed_color == COLORS[displayed_word], "nb_trial": (trial[0]["nb_trial"] + 1) if trial else 1},
+        trial_data = (
+            {
+                "displayed_word": displayed_word,
+                "color": displayed_color,
+                "congruent": displayed_color == COLORS[displayed_word],
+                "nb_trial": (trial[0]["nb_trial"] + 1) if trial else 1,
+            },
+        )
         start_time = time.time()
 
-        return displayed_word, {"color": displayed_color, "fontSize": "48px"}, trial_data, start_time
+        return (
+            displayed_word,
+            {"color": displayed_color, "fontSize": "48px"},
+            trial_data,
+            start_time,
+        )
 
     elif trigger in [f"btn-{c}" for c in COLORS.keys()]:
         print(f"Triggered by new color {trigger}\n")
@@ -80,12 +94,15 @@ def handle_event(color1, color2, color3, color4, current_res, trial, start_time)
         rt = (time.time() - start_time) * 1000  # en ms
         clicked_button = trigger.split("-")[1]
         displayed_color = trial[0]["color"]
-        correct = (COLORS[clicked_button] == displayed_color)
+        correct = COLORS[clicked_button] == displayed_color
 
         with open("./results.txt", "a") as fp:
-            fp.write(f"Temps de réponse;{rt};correct;{correct};displayed_word;{trial[0]['displayed_word']};displayed_color;{displayed_color};congruence;{trial[0]['congruent']};\n")
+            fp.write(
+                f"Temps de réponse;{rt};correct;{correct};displayed_word;{trial[0]['displayed_word']};displayed_color;{displayed_color};congruence;{trial[0]['congruent']};\n"
+            )
 
         return "", {}, trial, start_time
+
 
 @dash.callback(
     Output("redirect_goodbye", "pathname"),
